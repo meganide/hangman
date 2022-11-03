b43623cd9279d4efbf341ad62439ed461e512f70
Meganide <renashassan@hotmail.com>
2022-11-03 16:59:26 +0100

    refactored code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-//  För att toggla SVG:en
-
+// ************************************* Variables *************************************
 const figure = document.querySelector('figure');
 const resetbutton = document.querySelector('button');
 const mainContent = document.querySelector('main');
 const popup = document.querySelector('.popup');
+
+let time = 60;
 let countInterval;
 
 let hangman = ['scaffold', 'head', 'body', 'arms', 'legs'];
+
+let currentWord = [];
+let currentGuessed = [];
 
 let words = [
   'glad',
@@ -24,57 +28,31 @@ let words = [
   'antagonist',
 ];
 
-let currentWord = [];
-let currentGuessed = [];
-
-window.addEventListener('keypress', onKeyPress);
-
-function onKeyPress(e) {
-  if (!currentGuessed.includes(e.key)) {
-    currentGuessed.push(e.key);
-    if (currentGuessed.length === 1) {
-      startCounter();
-    }
-    drawLetter(e.key);
-    console.log(currentGuessed);
-
-    if (currentWord.includes(e.key)) {
-      currentWord.forEach((letter, index) => {
-        if (letter === e.key) {
-          document.querySelector(`[data-word-index='${index}']`).innerHTML =
-            letter;
-
-          let allCorrect = [];
-          currentWord.forEach((char) => {
-            const bool = currentGuessed.includes(char);
-            allCorrect.push(bool);
-          });
-          if (!allCorrect.includes(false)) {
-            popupScreen('Du vann!', 'darkgreen');
-          }
-        }
-      });
-    } else {
-      const bodyPart = hangman.shift();
-      figure.classList.add(bodyPart);
-      if (hangman.length === 0) {
-        popupScreen(
-          `Du förlorade! </br> Korrekt ord var: ${currentWord.join('')}`,
-          'darkred'
-        );
-      }
-    }
-  }
+// ************************************* Functions *************************************
+function randomizeWord() {
+  const index = Math.floor(Math.random() * words.length);
+  currentWord = [...words[index]];
+  words.splice(index, 1);
+  drawLetterUnderLine();
 }
 
-let time = 60;
+// Skapas en ledigt plats
+function drawLetterUnderLine() {
+  const boxes = document.querySelector('#boxes');
+  amountBoxes = currentWord.length;
 
-
+  for (let index = 0; index < amountBoxes; index++) {
+    let box = document.createElement('div');
+    box.setAttribute('data-word-index', index);
+    boxes.append(box);
+  }
+}
 
 function startCounter() {
   countInterval = setInterval(counter, 1000);
 }
 
+// Timers function som räknar tiden
 function counter() {
   time--;
   document.getElementById('counter').innerHTML = time;
@@ -87,32 +65,66 @@ function counter() {
   }
 }
 
-function randomizeWord() {
-  const index = Math.floor(Math.random() * words.length);
-  currentWord = [...words[index]];
-  words.splice(index, 1);
-  drawBoxes();
-  console.log(currentWord);
-}
+// Körs spelet när nån knapp trycks
+function onKeyPress(e) {
+  // Spara ned användarens tangentval och omvandla till en gemen
+  let letterPressed = e.key;
+  letterPressed = letterPressed.toLowerCase();
 
-function drawBoxes() {
-  const boxes = document.querySelector('#boxes');
-  amountBoxes = currentWord.length;
+  // Kontrollera om bokstaven användaren gissade på inte finns med i listan på bokstäverna som den redan gissat på
+  // och tillåt enbart bokstäver
+  if (
+    !currentGuessed.includes(letterPressed) &&
+    /^[a-ö]+$/i.test(letterPressed)
+  ) {
+    currentGuessed.push(letterPressed);
 
-  for (let index = 0; index < amountBoxes; index++) {
-    let box = document.createElement('div');
-    box.setAttribute('data-word-index', index);
-    boxes.append(box);
+    // Starta timern när användaren skriver första bokstaven
+    if (currentGuessed.length === 1) {
+      startCounter();
+    }
+
+    drawLetter(letterPressed);
+
+    // Om bokstaven användaren gissade på finns i det korrekta ordet
+    if (currentWord.includes(letterPressed)) {
+      // loopa igenom alla bokstäver i korrekt ord
+      currentWord.forEach((letter, index) => {
+        // Placera bokstaven på rätt plats
+        if (letter === letterPressed) {
+          document.querySelector(`[data-word-index='${index}']`).innerHTML =
+            letter;
+
+          // Kolla om man gissat rätt på alla bokstäver
+          let allCorrect = [];
+          currentWord.forEach((char) => {
+            const bool = currentGuessed.includes(char);
+            allCorrect.push(bool);
+          });
+
+          if (!allCorrect.includes(false)) {
+            popupScreen('Du vann!', 'darkgreen');
+          }
+        }
+      });
+      // Om man gissar på fel bokstav
+    } else {
+      // Hämta korrekt kroppsdel i rätt ordning
+      const bodyPart = hangman.shift();
+      figure.classList.add(bodyPart);
+
+      // Om det inte finns några kroppsdelar kvar har man förlorat
+      if (hangman.length === 0) {
+        popupScreen(
+          `Du förlorade! </br> Korrekt ord var: ${currentWord.join('')}`,
+          'darkred'
+        );
+      }
+    }
   }
 }
 
-function deleteElements(parent, child) {
-  const elements = document.querySelectorAll(`#${parent} ${child}`);
-  elements.forEach((element) => {
-    element.remove();
-  });
-}
-
+// Rita ut alla bokstäver användaren gissat på hittills.
 function drawLetter(guessedLetter) {
   const letters = document.querySelector('#letters');
   const letter = document.createElement('span');
@@ -120,8 +132,9 @@ function drawLetter(guessedLetter) {
   letters.append(letter);
 }
 
+// meddelande kommer om man har vunnit eller förlorat
 function popupScreen(text, color) {
-  document.getElementById('counter').innerHTML = "";
+  document.getElementById('counter').innerHTML = '60';
   clearInterval(countInterval);
   window.removeEventListener('keypress', onKeyPress);
   popup.querySelector('h2').innerHTML = text;
@@ -130,16 +143,8 @@ function popupScreen(text, color) {
   popup.style.background = color;
 }
 
-resetbutton.addEventListener('click', () => {
-  resetGame();
-  window.addEventListener('keypress', onKeyPress);
-  mainContent.classList.toggle('hide');
-  popup.classList.toggle('hide');
-  time = 60;
-});
-
+// Resetta alla variabler och hämta nytt random ord.
 function resetGame() {
-  console.log('du förlorade');
   hangman = ['scaffold', 'head', 'body', 'arms', 'legs'];
   figure.className = '';
   currentGuessed = [];
@@ -149,4 +154,24 @@ function resetGame() {
   randomizeWord();
 }
 
+//Används för att ta bort bokstäver som användare har matat in
+function deleteElements(parent, child) {
+  const elements = document.querySelectorAll(`#${parent} ${child}`);
+  elements.forEach((element) => {
+    element.remove();
+  });
+}
+
+//************************************* Event Listeners *************************************
+window.addEventListener('keypress', onKeyPress);
+
+resetbutton.addEventListener('click', () => {
+  resetGame();
+  window.addEventListener('keypress', onKeyPress);
+  mainContent.classList.toggle('hide');
+  popup.classList.toggle('hide');
+  time = 60;
+});
+
+// ************************************* Function Calls *************************************
 randomizeWord();
